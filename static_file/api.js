fb203b24351acb49acbb8380e6d8e64f8297ab7f
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-02-27 07:51:00 +0530

    api data

---
+++ static_file/api.js
@@ -0,0 +1,37 @@
+async function js_function(){
+	let repo_link=document.getElementById('github_link').value;
+	let api_link="https://distributed-compiler.herokuapp.com/";
+	let api_repo_link= api_link+"api/add/?link="+repo_link;
+
+	document.write("hello "+api_repo_link+"<br>");
+	let response = await fetch(api_repo_link);
+
+	if(response.ok){
+		let json=await response.json();
+		let id=json['id'];
+		alert(id);
+		let api_status=api_link+"api/getStatus/?id="+id;
+		let status_response=await fetch(api_status);
+		if(status_response.ok){
+			let status_text=await status_response.text();
+			status_text=status_text.split(":",2);
+			let status=status_text[1][1];
+			if(status=='-')
+				status="-1";
+			if(status=='0')
+			{
+				while(status!='1')
+				{
+					status_text=await status_response.text();
+					status_text=status_text.split(":",2);
+					status=status_text[1][1];
+				}
+			}
+			document.write("status is "+status+"<br>");
+		}
+	}
+	else{
+		document.write("HTTPS-Error: "+response.status)
+	}
+	
+}
